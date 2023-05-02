3f258d8c1d0212fa96c532482691302cb53cb848
mustafa <mustafamcm9@gmail.com>
2023-05-02 17:08:35 +0300

    create window class

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.root_widget = Tk()
+        self.__root = Tk()
+        self.__root.title = "Maze Solver"
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
+        self.__is_window_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-  
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__is_window_running = True
+        while self.__is_window_running:
+            self.redraw()
+
+    def close(self):
+        self.__is_window_running = False
+
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+main()
