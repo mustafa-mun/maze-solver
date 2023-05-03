da5a2679a5a2879cef229ddb9b2b885eb31484da
mustafa <mustafamcm9@gmail.com>
2023-05-03 19:13:15 +0300

    add default fill color

--- modules/window.py
+++ modules/window.py
@@ -21,5 +21,5 @@ class Window:
     def close(self):
         self.__is_window_running = False
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color = "black"):
         line.draw(self.__canvas, fill_color)
