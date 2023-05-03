274296d55bfea16710d00e9bab64b43334f87490
mustafa <mustafamcm9@gmail.com>
2023-05-03 23:50:27 +0300

    gui module   Add gui element classes on it's own module called "gui.py"

---
+++ modules/gui.py
@@ -0,0 +1,46 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title = "Maze Solver"
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__is_window_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__is_window_running = True
+        while self.__is_window_running:
+            self.redraw()
+
+    def close(self):
+        self.__is_window_running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_one, point_two):
+        self.point_one = point_one
+        self.point_two = point_two
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point_one.x,
+            self.point_one.y,
+            self.point_two.x,
+            self.point_two.y,
+            fill=fill_color,
+            width=2,
+        )
+        canvas.pack(fill=BOTH, expand=1)
