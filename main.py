9bdd2723679f49dec75f6da2e9cee5d8a6778a1a
mustafa <mustafamcm9@gmail.com>
2023-05-03 21:03:31 +0300

    maze class

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from modules.window import Window
 from modules.cell import Cell
+from modules.maze import Maze
+
 
 def main():
     win = Window(800, 600)
-    cell_one = Cell(True, False, True, True, 10, 100, 50, 100, win)
-    cell_one.draw()
-
-    cell_two = Cell(False, True, True, True, 300, 500, 200, 400, win)
-    cell_two.draw()
-
-    cell_three = Cell(True, True, False, True, 200, 400, 50, 150, win)
-    cell_three.draw()
-
-    cell_one.draw_move(cell_two)
-    cell_three.draw_move(cell_one)
-    cell_three.draw_move(cell_two)
+    maze = Maze(20, 20, 3, 3, 50, 50, win)
     win.wait_for_close()
 
 
---
+++ modules/maze.py
@@ -0,0 +1,53 @@
+from modules.cell import Cell
+import time
+
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self.__x1_temp = x1  # this needs a better solution
+        self.__y1_temp = y1  # this needs a better solution
+        self.__create_cells()
+
+    def __animate(self):
+        if self.__win is None:
+            return
+        self.__win.redraw()
+        time.sleep(0.05)
+
+    def __draw_cell(self, i, j):
+        if self.__win is None:
+            return
+        x1 = self.__x1 + i * self.__cell_size_x
+        y1 = self.__y1 + j * self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self.__animate()
+
+    def __create_cells(self):
+        cell_list = []
+        # populate cell_list
+        for i in range(9):
+            cell_list.append(Cell(self.__win))
+
+        # create 2d cell list
+        for i in range(self.__num_cols + 1):
+            list = []
+            for j in range(self.__num_rows + 1):
+                list.append(cell_list[j])
+            self.__cells.append(list)
+
+        # draw cells
+        for i in range(len(self.__cells)):
+            for j in range(len(self.__cells)):
+                self.__draw_cell(i, j)
+            self.__y1 = self.__y1_temp
+            self.__x1 = self.__x1_temp
