8618de0166f5ba2485c4192680500d644b3edd6f
mustafa <mustafamcm9@gmail.com>
2023-05-04 16:51:34 +0300

    track cell visited

--- modules/cell.py
+++ modules/cell.py
@@ -7,6 +7,7 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self.__x1 = None
         self.__x2 = None
         self.__y1 = None
